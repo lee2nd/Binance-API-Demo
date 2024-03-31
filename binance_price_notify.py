94b4039c7b471310b86d37a312c405734610b816
Lee <48121658+lee2nd@users.noreply.github.com>
2024-03-31 20:04:39 +0800

    Update binance_price_notify.py

--- binance_price_notify.py
+++ binance_price_notify.py
@@ -1,17 +1,17 @@
-# I will be amillionaire
 # Line Token applied from https://notify-bot.line.me/zh_TW/
 # Binance API Token applied from https://algotrading101.com/learn/binance-python-api-guide/
 
 from binance.client import Client
 import requests
 import time
+import socket
 
 class Binance():
     
     def __init__(self):
         
-        self.api_key = 'YOUR_API_KEY'
-        self.api_secret = 'YOUR_API_SECRET'
+        self.api_key = 'MYgUFxFMjeVglbzUsips2x38QR11XNJQ5NuYVbov3QBITOcyYmnBGA3MtuHTegWY'
+        self.api_secret = 'tOfsgUFcmjb1odifv2F1z4gYjl8Fvbzr5xjbQmiMbNSZnADjRcpCuToHp1H0R2x7'
         self.client = Client(self.api_key,self.api_secret)
 
     def check_price(self, symbol, interval, drop_percentage):
@@ -32,12 +32,12 @@ class Binance():
 def line_notify(message):
     
     headers = {
-        "Authorization": "Bearer YOUR_TOKEN", 
+        "Authorization": "Bearer UAg4tVzgqcgfIvu6cJWvbel2d2VNQPop3v2O0KUGmzI", 
         "Content-Type" : "application/x-www-form-urlencoded"
     }
 
     payload = {'message': message}
-    r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+    r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload, timeout=5)
     
     return r.status_code
 
@@ -54,20 +54,29 @@ drop_pctg_lst = [0.5,    # 3 minutes
                 1.25,    # 4 hours
                 1.5      # 8 hours
                 ]
+retry_counter = 0
 
 # initialize object
 b_obj = Binance()
 
-while True:
-    
-    time.sleep(3)
-    
-    for symbol in sbl_lst:
+while retry_counter < 5:
+    try:    
         
-        for interval, drop_percentage in zip(itvl_lst,drop_pctg_lst):
+        time.sleep(3)  
+        
+        for symbol in sbl_lst:
             
-            # generate the price movement messages
-            message = b_obj.check_price(symbol, interval, drop_percentage)
-            
-            # send line notify message
-            line_notify(message)   
+            for interval, drop_percentage in zip(itvl_lst,drop_pctg_lst):
+                
+                # generate the price movement messages
+                message = b_obj.check_price(symbol, interval, drop_percentage)
+                
+                # send line notify message
+                line_notify(message)
+                
+    except socket.error as error:
+        
+        print("Connection Failed due to socket - {}").format(error)
+        print("Attempting {} of 5").format(retry_counter)
+        time.sleep(3)
+        retry_counter += 1
